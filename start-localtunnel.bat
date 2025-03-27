dcba1ca34ae85901c9db0b4ea49b5b8143979520
Andre Mesquita <avmesquita@gmail.com>
2025-03-27 01:13:11 +0000

    - inclusão do package.json para simplificar a instalação dos pacotes necessários; - inclusão do batch "start-ngrok.bat" para exposição do site através do NGROK - inclusão do batch "start-localtunnel.bat" para exposição do site através do LocalTunnel

---
+++ start-localtunnel.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+@ECHO LOCALTUNNEL (https://theboroer.github.io/localtunnel-www)
+SET bypass-tunnel-reminder=1
+lt --port 8088
---
+++ start-ngrok.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+@ECHO NGROK (https://dashboard.ngrok.com/get-started/setup/windows)
+set ngrok-skip-browser-warning=1
+ngrok http http://localhost:8080
